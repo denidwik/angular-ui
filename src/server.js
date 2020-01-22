961c59cd79605648110b6a94c102ab7c59c821c5
denni.kurniawan <denni.kurniawan@jatis.com>
2020-01-22 17:35:53 +0700

    a

--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/web-uii'));
+app.use(express.static(__dirname + '/src/web-uii'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/web-uii/index.html'));
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
 // default Heroku PORT
